9435e1b0675223daea5deaced4ad2ab3fb7fc2ee
Afonso Henrique <henriquexd_96@hotmail.com>
2022-05-28 11:40:29 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -13,7 +13,8 @@ namespace SalesWebMvc.Controllers {
         }
 
         public IActionResult About() {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC app from C# course";
+            ViewData["Aluno"] = "Afonso Henrique";
 
             return View();
         }
